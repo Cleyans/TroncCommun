4ec2d77bc70c10f8e33479548c21146d6df95276
Bryan Clement <138831731+Cleyans@users.noreply.github.com>
2023-10-16 13:38:09 +0200

    Update ft_memset.c

--- Libf/ft_memset.c
+++ Libf/ft_memset.c
@@ -3,7 +3,7 @@ void *ft_memset(void *string, int valeur, size_t taille)
     unsigned char *temp = (unsigned char *)string;
     size_t compteur = 0;
 
-    while (i < taille)
+    while (compteur < taille)
     {
         *temp = (unsigned char)valeur;
         ++temp;

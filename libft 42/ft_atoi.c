d026b3c832600cd386a550d7684a7ccb6e65779b
Bryan Clement <brclemen@c1r4p12.42lehavre.fr>
2023-10-18 15:57:27 +0200

    atoiadd

---
+++ libft 42/ft_atoi.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brclemen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 14:36:59 by brclemen          #+#    #+#             */
+/*   Updated: 2023/10/18 14:37:01 by brclemen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	size_t	index;
+	int	sign;
+	int	result;
+	
+	index = 0;
+	sign = 1;
+	result = 0;
+	while (nptr[index] == '-' || nptr[index] == '+')
+	{
+		if (nptr[index] == '-')
+		{
+			sign *= -1;
+		}
+		index++;
+	}
+	while (nptr[index] >= '0' && nptr[index] <= '9')
+	{
+		result = result * 10 + (nptr[index] - '0');
+		index++;
+	}
+	return (result * sign);
+}
+
+#include <stdio.h>
+int main()
+{
+    char str[] = "-+897h005429";
+ 
+    int val = ft_atoi(str);
+    printf("%d\n", val);
+}
